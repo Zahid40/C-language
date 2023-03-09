856a1c39d03984c309356d4d4f18977e6b749a3e
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-03-09 12:45:47 +0530

    .

--- 029-Call-by-Value-and-Call-by-Refence.c
+++ 029-Call-by-Value-and-Call-by-Refence.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
-int jj(int a , int b){
-    return a+b;
+// call by Value 
+
+// int jj(int a, int b)
+// {
+//     return a + b;
+// }
+// int main()
+// {
+//     int x = 3, y = 2;
+//     int h = jj(x, y);
+//     printf("%d", h);
+//     return 0;
+// }
+
+// Call by Reference
+
+void alter_Values_(int *x , int *y){
+     *x = *x + *y;
+     *y = *x - *y;
+     return;
 }
+
 int main()
 {
-    int x=3,y=2;
-    int h =jj(x,y);
-    printf("%d" , h);
+    int a = 4 , b = 2;
+    printf("The Value of a is %d and b is %d\n" , a , b );
+    alter_Values_(&a,&b);
+    printf("The New Value of a is %d and b is %d\n" , a , b );
+
     return 0;
 }
