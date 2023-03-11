fc420cbee490428ea1d17dbfd220057682b373c3
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-03-11 22:52:28 +0530

    .

--- 028-Exercise-4-Star-Patterns.c
+++ 028-Exercise-4-Star-Patterns.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int printstar(int rows){
+int printstar(int rows)
+{
+    
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j <= i; j++)
@@ -9,19 +11,73 @@ int printstar(int rows){
         }
         printf("\n");
     }
-    
     return 0;
 }
 
+int Reverse_printstar(int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j <= rows - i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+int superstar(int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
+
+        for (int j = 0; j < rows - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+        for (int j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < rows - i - 1; j++)
+        {
+            printf("*");
+        }
+        for (int j = 0; j < rows - i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 int main()
 {
     int rows;
     for (int i = 0; i < 10; i++)
     {
-    printf("How MAny rows you Want = ");
-    scanf("%d" , &rows);
-    printstar(rows);
+        printf("How MAny rows you Want = ");
+        scanf("%d", &rows);
+        // printstar(rows);
+        // Reverse_printstar(rows);
+        superstar(rows);
     }
     return 0;
+    
 }
