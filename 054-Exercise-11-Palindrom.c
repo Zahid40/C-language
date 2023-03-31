02fcafb4b1339d0861bf5afcbd8c454ad7ffa061
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-03-31 14:49:54 +0530

    .

---
+++ 054-Exercise-11-Palindrom.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int Plaindrom(int a){
+    
+}
+
+int main()
+{
+    int number;
+    printf("Enter the Number :");
+    scanf("%d",&number);
+    if (palindrom(number))
+    {
+        printf("The Number is Plaindrom");
+    }
+    else{
+        printf("The Number is Not Palindrom");
+    }
+    return 0;
+}
