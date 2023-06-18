feccac868cef31d993f6e07b72c76e93f569f64e
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-06-18 12:43:30 +0530

    .

---
+++ semread.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#define fof(n) (5*pow(n,3) + (2*pow(n,1)) + 3 )
+
+int main (){
+
+    float n;
+
+    printf("Enter a number:\n");
+    scanf("%lf", &n);
+
+    printf("%.2f\n\n", fof(n));
+
+    return (0);
+}
